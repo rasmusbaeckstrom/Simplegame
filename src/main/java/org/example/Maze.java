2ed076348a09eaa9f8d2e37492f824a34c679902
Rasmus Bäckström <rasmusbaeckstrom@gmail.com>
2024-09-04 10:12:24 +0200

    Added class Maze and subclass Sword

--- src/main/java/org/example/Maze.java
+++ src/main/java/org/example/Maze.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Maze {
+    private ArrayList<String> structure;
+
+    public Maze() {
+        this.structure = new ArrayList<>();
+    }
+
+    public void addElement(String element) {
+        structure.add(element);
+    }
+
+    public String getElement(int index) {
+        return structure.get(index);
+    }
+
+    public ArrayList<String> getStructure() {
+        return structure;
+    }
+
+    public void setStructure(ArrayList<String> structure) {
+        this.structure = structure;
+    }
 }
